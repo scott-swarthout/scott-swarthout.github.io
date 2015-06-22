009b7c23859c8bc8760ee9b457cdcecab4ddaef8
Scott Swarthout <swarthout@users.noreply.github.com>
2015-06-22 11:21:31 -0400

    change username

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,7 +53,7 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "scott-swarthout", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "swarthout", selector: "#ghfeed" });
 
 
 });
